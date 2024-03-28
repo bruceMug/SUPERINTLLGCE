12fe6e41b1abd19fa406ff4b4df69f307dac93b2
brucemug <mugzbru199@gmail.com>
2024-03-28 21:49:14 +0300

    created welcome screen

---
+++ Tracy/src/screens/WelcomeScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const WelcomeScreen = () => {
+  return (
+    <View>
+      <Text>Welcome to the app!</Text>
+    </View>
+  );
+};
+
+export default WelcomeScreen;
