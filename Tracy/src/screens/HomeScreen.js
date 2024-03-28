e57dbc7fac8005fc2c4d913c628df36fe550c522
brucemug <mugzbru199@gmail.com>
2024-03-28 22:56:29 +0300

    added top icon

--- Tracy/src/screens/HomeScreen.js
+++ Tracy/src/screens/HomeScreen.js
@@ -1,10 +1,31 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Image, SafeAreaView } from "react-native";
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from "react-native-responsive-screen";
 
 const HomeScreen = () => {
+  const [messages, setMessages] = useState([2]);
   return (
-    <View>
-      <Text>Welcome to the Home Screen!</Text>
+    <View style={{ flex: 1, backgroundColor: "white" }}>
+      <SafeAreaView style={{ flex: 1, marginTop: 15 }}>
+        <View style={{ flexDirection: "row", justifyContent: "center" }}>
+          <Image
+            source={require("../../assets/images/Blue-eye.jpg")}
+            style={{ width: hp(20), height: hp(20), borderRadius: 100 }}
+          />
+        </View>
+
+        {/* features || text message history */}
+        {messages.length > 0 ? (
+          <View>
+            <Text>Empty</Text>
+          </View>
+        ) : (
+          <Features />
+        )}
+      </SafeAreaView>
     </View>
   );
 };
