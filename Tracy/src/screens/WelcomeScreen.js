758c79fb6eea786a879388715a54722cf3a055ca
brucemug <mugzbru199@gmail.com>
2024-03-28 22:29:58 +0300

    added the UI

--- Tracy/src/screens/WelcomeScreen.js
+++ Tracy/src/screens/WelcomeScreen.js
@@ -1,11 +1,90 @@
 import React from "react";
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 
 const WelcomeScreen = () => {
   return (
-    <View>
-      <Text>Welcome to the app!</Text>
-    </View>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        backgroundColor: "white",
+        alignItems: "center",
+      }}
+    >
+      <View
+        style={{
+          marginBottom: 50,
+          alignItems: "center",
+          padding: 20,
+        }}
+      >
+        <Text
+          style={{
+            fontWeight: "bold",
+            color: "#4A5568",
+            fontSize: 36,
+          }}
+        >
+          TR∀CY
+        </Text>
+        <Text
+          style={{
+            fontWeight: "700",
+            textAlign: "center",
+            flexDirection: "row",
+            color: "#718096",
+            justifyContent: "center",
+            letterSpacing: 1,
+          }}
+        >
+          The future is here, powered by AI.
+        </Text>
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "center",
+          padding: 10,
+          marginBottom: 70,
+        }}
+      >
+        <Image
+          source={require("../../assets/images/Blue-eye.jpg")}
+          style={{ width: 300, height: 300, borderRadius: 200 }}
+        />
+      </View>
+
+      <TouchableOpacity
+        style={{
+          backgroundColor: "#047857",
+          padding: 10,
+          borderRadius: 10,
+          marginTop: 15,
+          // flex: 1,
+          // flexDirection: "row",
+          width: "85%",
+          position: "absolute",
+          bottom: "5%",
+        }}
+      >
+        <Text
+          style={{
+            textAlign: "center",
+            fontWeight: "bold",
+            color: "white",
+            fontSize: 20,
+          }}
+        >
+          Get Started
+        </Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 };
 
