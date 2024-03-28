f60fba7159928134672e0fdc2789f0fb99029880
brucemug <mugzbru199@gmail.com>
2024-03-28 23:45:03 +0300

    dummy messages

---
+++ Tracy/src/constants/index.js
@@ -0,0 +1,16 @@
+export const dummyMessages = [
+  {
+    role: "user",
+    content: "How are you?",
+  },
+  {
+    role: "assistant",
+    content: "I'm fine, How may I help you today.",
+  },
+  { role: "user", content: "create an image of a dog playing with cat." },
+  {
+    role: "assistant",
+    content:
+      "https://cdn.pixabay.com/photo/2016/02/19/15/46/labrador-retriever-1210559_640.jpg",
+  },
+];
